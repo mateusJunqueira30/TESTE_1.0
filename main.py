59cc85fd5799280bb16b503d84abf86fa5c0c799
mateusJunqueira30 <mateusjlima2011@gmail.com>
2026-05-15 17:51:44 -0300

    Create main.py
    #há um erro, além de não estar completo.

---
+++ main.py
@@ -0,0 +1,12 @@
+dict = {"STALKEAR": "investigar a vida de alguém online",
+        "CRINGE": "algo vergonhoso ou constrangedor",
+        "VDD": "abreviação da palavra verdade",
+        "BISCOITAR": "postar algo apenas para chamar a atenção",
+        "HATER": "pessoa que está constantemente criticando os outros",
+        "VLW": "abreviação da palavra valeu",
+        "FLOP": "Algo que falhou ou fracassou"}
+
+q1 = input("digite uma giria atual, em letras maiusculas para saber o significado")
+
+if q1 in dict.keys():
+           print(dict.keys[q1])
