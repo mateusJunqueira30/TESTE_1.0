835ae87615702dc2772aa66642f7d3e996343c41
mateusJunqueira30 <mateusjlima2011@gmail.com>
2026-05-22 17:33:24 -0300

    Update 1_projeto.py

--- 1_projeto.py
+++ 1_projeto.py
@@ -1,1 +1,13 @@
+import random
+
+caracteres = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+
+quantidade = int(input("qual será a quantidade de caractéres da sua senha? "))
+
+senha = ""
+
+for i in range (quantidade):
+    senha += random.choice(caracteres)
+
+print("Essa é sua senha: " + senha)
 
